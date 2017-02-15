a8f20523f59ff68b45f5b716fc96c35e051af472
amanda973 <1136447460@qq.com>
2017-02-15 19:46:13 +0800

    add

--- Weather2/app/src/main/java/com/list/asus/weather2/ChooseActivity.java
+++ Weather2/app/src/main/java/com/list/asus/weather2/ChooseActivity.java
@@ -1,13 +1,35 @@
 package com.list.asus.weather2;
 
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class ChooseActivity extends AppCompatActivity {
+
+    public DrawerLayout drawerLayout;
+    private TextView addSlipTextview;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_choose);
+
+        initView();
+    }
+
+    private void initView() {
+        drawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
+        addSlipTextview = (TextView) findViewById(R.id.add_slip_textview);
+
+        addSlipTextview.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                drawerLayout.openDrawer(GravityCompat.START);
+            }
+        });
     }
 }
