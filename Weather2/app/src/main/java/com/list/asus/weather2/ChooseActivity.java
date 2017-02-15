1700a9217be1345dcf72836afc4a8c69b0d03867
Hesheng <1451238152@qq.com>
2017-02-15 19:34:35 +0800

    add backText

--- Weather2/app/src/main/java/com/list/asus/weather2/ChooseActivity.java
+++ Weather2/app/src/main/java/com/list/asus/weather2/ChooseActivity.java
@@ -1,13 +1,22 @@
 package com.list.asus.weather2;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 
 public class ChooseActivity extends AppCompatActivity {
+
+    public static void actionStart(Context context) {
+        Intent intent = new Intent(context, ChooseActivity.class);
+        context.startActivity(intent);
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_choose);
+
     }
+
 }
